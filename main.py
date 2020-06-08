5f26a2032203aab32340c1ada6bd8626bc399f36
Michał Kowieski <michael.kowieski@gmail.com>
2020-06-08 12:49:48 +0200

    Add model endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -10,3 +11,20 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {'id': item_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def read_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {'model_name': model_name, 'message': 1}
+    elif model_name == ModelName.resnet:
+        return {'model_name': model_name, 'message': 2}
+    elif model_name == ModelName.lenet:
+        return {'model_name': model_name, 'message': 3}
+        
